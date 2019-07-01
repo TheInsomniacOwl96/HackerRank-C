6e944cec716eee2799cf4de8565db3357b734249
luther <luther1096@gmail.com>
2019-07-01 20:27:18 +0530

    updated

--- C-HackerRank-Programs/prog17.c
+++ C-HackerRank-Programs/prog17.c
@@ -51,3 +51,21 @@ int main() {
 
     return 0;
 }
+
+
+/*
+ALTERNATIVELY
+int marks_summation(int* marks, int number_of_students, char gender) {
+    int sum = 0;
+    for (int i = (gender == 'b' ? 0 : 1); i < number_of_students; i = i + 2)
+        sum += *(marks + i);
+
+    return sum;
+}
+*/
+
+// OR
+
+/*
+int i = !(gender == 'b');
+*/
